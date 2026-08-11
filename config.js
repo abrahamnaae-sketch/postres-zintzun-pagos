26bd68fb3804dbe2a0adafe0a7a5a2d04f8e4ef3
abrahamnaae-sketch <abraham.naae@gmail.com>
2026-08-10 19:14:19 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ZINTZUN_CONFIG = {
+  whatsappNumber: "524613241712",
+  whatsappMessage: "Hola, necesito ayuda con mi pedido o pago de Postres Zintzún.",
+  receiptMessage: "Hola, envío mi comprobante de pago de Postres Zintzún."
+};
