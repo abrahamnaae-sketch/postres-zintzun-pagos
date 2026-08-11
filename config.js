cd866ca075f6c92430ed68f25b6f50603ba77eeb
abrahamnaae-sketch <abraham.naae@gmail.com>
2026-08-10 19:13:05 -0600

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.ZINTZUN_CONFIG = {
-  whatsappNumber: "524613241712",
-  whatsappMessage: "Hola, necesito ayuda con mi pedido o pago de Postres Zintzún.",
-  receiptMessage: "Hola, envío mi comprobante de pago de Postres Zintzún."
-};
